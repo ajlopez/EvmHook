d4f01a54add1e0c7af763ce004252a5efbc42ee0
ajlopez <webmaster@ajlopez.com>
2017-09-24 12:10:19 -0300

    Improving first run code test

--- test/runcode.js
+++ test/runcode.js
@@ -8,6 +8,11 @@ exports['run simple code without errors'] = function (test) {
     
     vm.runCode('6001', function (err, data) {
         test.ok(!err);
+        test.ok(data);
+        test.ok(data.runState);
+        test.ok(data.runState.stack);
+        test.equal(data.runState.stack.length, 1);
+        test.equal(data.runState.stack[0].toString('hex'), '0000000000000000000000000000000000000000000000000000000000000001');
         test.done();
     });
 }
